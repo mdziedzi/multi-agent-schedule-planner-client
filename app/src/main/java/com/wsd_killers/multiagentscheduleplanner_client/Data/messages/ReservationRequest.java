38d8e1a4841a698172c58d82bff1a6b027c25242
mdziedzi <marcindziedzic8@gmail.com>
2018-12-09 16:35:35 +0100

    Add 2 messages classes.

---
+++ app/src/main/java/com/wsd_killers/multiagentscheduleplanner_client/Data/messages/ReservationRequest.java
@@ -0,0 +1,71 @@
+package com.wsd_killers.multiagentscheduleplanner_client.data.messages;
+
+import com.wsd_killers.multiagentscheduleplanner_client.Exceptions.negativeValueException;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+
+public class ReservationRequest {
+    public Date beginHour;
+    public Date endHour;
+    public int slots;
+
+
+    public ReservationRequest() {
+        beginHour = null;
+        endHour = null;
+        slots = 0;
+    }
+
+    public ReservationRequest(Date bHour, Date eHour, int slotsNumber) throws negativeValueException {
+        if (slots <= 0)
+            throw new negativeValueException("Slots should be greater than zero: " + slots);
+        slots = slotsNumber;
+        beginHour = bHour;
+        endHour = eHour;
+    }
+
+    public static String serialize(ReservationRequest data) {
+        String serializedObject = "";
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            ObjectOutputStream so = new ObjectOutputStream(bo);
+            so.writeObject(data);
+            so.flush();
+            final byte[] byteArray = bo.toByteArray();
+            serializedObject = android.util.Base64.encodeToString(byteArray, android.util.Base64.DEFAULT);
+
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        return serializedObject;
+    }
+
+    public static ReservationRequest deserialize(String serializedReservationRequest) {
+        ReservationRequest s = null;
+        try {
+            final byte[] bytes = android.util.Base64.decode(serializedReservationRequest, android.util.Base64.DEFAULT);
+            byte b[] = serializedReservationRequest.getBytes();
+            ByteArrayInputStream bi = new ByteArrayInputStream(bytes);
+            ObjectInputStream si = new ObjectInputStream(bi);
+            s = (ReservationRequest) si.readObject();
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        return "ReservationRequest{" +
+                "beginHour=" + beginHour +
+                ", endHour=" + endHour +
+                ", slots=" + slots +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/wsd_killers/multiagentscheduleplanner_client/Data/messages/ReservationResponse.java
@@ -0,0 +1,49 @@
+package com.wsd_killers.multiagentscheduleplanner_client.data.messages;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class ReservationResponse {
+
+    public int id; // id = -1 - the reservation is not possible, read explanation to see why
+    public String explanation;
+
+    public ReservationResponse(int id, String explanation) {
+        this.id = id;
+        this.explanation = explanation;
+    }
+
+    public static String serialize(ReservationResponse data) {
+        String serializedObject = "";
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            ObjectOutputStream so = new ObjectOutputStream(bo);
+            so.writeObject(data);
+            so.flush();
+            final byte[] byteArray = bo.toByteArray();
+            serializedObject = android.util.Base64.encodeToString(byteArray, android.util.Base64.DEFAULT);
+
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        return serializedObject;
+    }
+
+    public static ReservationResponse deserialize(String serializedReservationRequestData) {
+        ReservationResponse s = null;
+        try {
+            final byte[] bytes = android.util.Base64.decode(serializedReservationRequestData, android.util.Base64.DEFAULT);
+            byte b[] = serializedReservationRequestData.getBytes();
+            ByteArrayInputStream bi = new ByteArrayInputStream(bytes);
+            ObjectInputStream si = new ObjectInputStream(bi);
+            s = (ReservationResponse) si.readObject();
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        return s;
+    }
+}
